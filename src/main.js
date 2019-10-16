bb9f502ea37ea568abcfbc6c7563a32b872ea4d2
KaterynaKostrubova <katerinakostrubova@gmail.com>
2019-10-16 14:15:03 +0300

    first

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false //отключает предупреждение о работе в режиме разработки при запуске Vue
 
 new Vue({
   render: h => h(App),
